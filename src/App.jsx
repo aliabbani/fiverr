474884d8d9ad572340ee78168335475600b70683
Ali Abbani <aliabbani08@gmail.com>
2023-03-15 12:58:02 +0100

    add navBar and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,72 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Gigs from "./pages/gigs/Gigs";
+import MyGigs from "./pages/myGigs/MyGigs";
+import Orders from "./pages/orders/Orders";
+import Messages from "./pages/messages/Messages";
+import Message from "./pages/message/Message";
+import Add from "./pages/add/Add";
+import Gig from "./pages/gig/Gig";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import "./app.scss";
 
 function App() {
+  const Layout = () => {
+    return (
+      <div className="app">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    );
+  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/gigs",
+          element: <Gigs />,
+        },
+        {
+          path: "/myGigs",
+          element: <MyGigs />,
+        },
+        {
+          path: "/orders",
+          element: <Orders />,
+        },
+        {
+          path: "/messages",
+          element: <Messages />,
+        },
+        {
+          path: "/message/:id",
+          element: <Message />,
+        },
+        {
+          path: "/add",
+          element: <Add />,
+        },
+        {
+          path: "/gig/:id",
+          element: <Gig />,
+        },
+      ],
+    },
+  ]);
+
   return (
     <div>
-      <Navbar />
+      <RouterProvider router={router} />
+      {/* <div>hi</div>
       <div>hi</div>
       <div>hi</div>
       <div>hi</div>
@@ -64,8 +126,7 @@ function App() {
       <div>hi</div>
       <div>hi</div>
       <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>v
+      <div>hi</div> */}
     </div>
   );
 }
